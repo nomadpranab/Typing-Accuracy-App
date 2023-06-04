2d5a1a3863789a1841d161478fc5b605a4348006
PRANAB PATRA <pranabpatra520@gmail.com>
2023-06-04 10:12:56 +0530

    commit

--- typer/src/App.js
+++ typer/src/App.js
@@ -1,71 +1,75 @@
-import React, { useState } from 'react';
-import Quote from './Quote';
+import React, { useState, useEffect } from "react";
+import './Style.css';
 
 function TypingSpeedApp() {
-  const getquote=async()=>{
-    let response=await fetch ("https://type.fit/api/quotes");
-    let data=response.json();
-    console.log(data);
-    
-  }
-  getquote();
-
-  const text=("Hello World");
-  const [input, setInput] = useState('');
+  const [text, settext] = useState("");
+  const [input, setInput] = useState("");
   const [accuracy, setAccuracy] = useState(0);
-  const [count,setCount]=useState(0);
-  const [button,setbutton]=useState('btn btn-dark disabled');
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const sentence = async () => {
+      let response = await fetch("https://type.fit/api/quotes");
+      const data = await response.json();
+      let index = Math.floor(Math.random() * 1000);
+      settext(data[index].text);
+    };
+    sentence();
+  }, []);
 
   const handleInputChange = (event) => {
     const inputValue = event.target.value;
     setInput(inputValue);
-    if(inputValue===text)
-    {
-      setbutton('btn btn-dark'); 
-    }
-    else
-    {
-      setbutton('btn btn-dark disabled');
-    }
-
   };
-
 
   //Calculation Of accuracy
 
-  const calc=(event)=>{
-    if(event.key==='Backspace'){
-      setCount(count+1);
+  const calc = (event) => {
+    if (event.key === "Backspace") {
+      setCount(count + 1);
     }
-    setAccuracy(100-(Math.round(count/text.length *100)));
-  }
-
-
-
-  const handleShowResult = () => {
-    alert(`Accuracy: ${accuracy}%`);
+    setAccuracy(100 - Math.round((count / text.length) * 100));
   };
-  
 
   return (
-    <div>
+    <div id="body" className="text-center">
+      <h1 className="text-center p-5" style={{color:"#F6F1F1"}}> Hey Coder !</h1>
+      
 
-      <Quote/>
-      {/* <h1>Typing Speed App</h1>
-      <p>{text}</p>
-      <input type="text" value={input} onChange={handleInputChange} onKeyDown={calc} />
-      <button className={button} onClick={handleShowResult}>Show More Results</button>
-      <br></br>
-      <sample>You will get the result once you enter the correct input</sample>
-      <h4>Your accuracy is : </h4>
+      <div className="card mb-4 mt-5 m-auto" style={{width:"16rem",backgroundColor:"#F6F1F1"}}>
+        <div className="card-body pb-3">
+          <h5 className="card-title">Hi!</h5>
+          <h6 className="card-subtitle mb-2 text-muted">Quote of the day</h6>
+          <p className="card-text">
+            {text}
+          </p>
+        </div>
+      </div>
+
+      <div>
+        <h6 className="mt-3" style={{color:"#F6F1F1"}}>Repeat the same below to test your typing skills</h6>
+      </div>
+
+      <div>
+      <textarea className="mt-3 mb-3" style={{backgroundColor:"#F6F1F1"}}
+      rows="4" cols="60"
+        type="text"
+        value={input}
+        onChange={handleInputChange}
+        onKeyDown={calc}
+      />
+      </div>
+      <div style={{color:"#FEFF86"}}>You will get the result once you enter the correct input</div>
+     
       
-      
-      {input===text && (
+      <h4 className="mt-3" style={{color:"#F6F1F1"}}>Your accuracy is : </h4>
+
+      {input === text && (
         <>
-            <span>{accuracy}</span>
-            <br></br>
+          <span>{accuracy}</span>
+          <br></br>
         </>
-      )} */}
+      )}
     </div>
   );
 }
